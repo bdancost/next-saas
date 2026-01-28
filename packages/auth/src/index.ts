a886a026c5de1965265fb7558b96acd8d98356be
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2026-01-28 18:26:46 -0300

    feat:Otimizando a tipagem das permissoes

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -2,10 +2,23 @@ import { AbilityBuilder, createMongoAbility } from '@casl/ability'
 import type { CreateAbility, MongoAbility } from '@casl/ability'
 import { permissions } from './permissions'
 import type { User } from './models/user'
-import type { UserSubject } from './subjects/user'
-import type { ProjectSubject } from './subjects/project'
+import { userSubject } from './subjects/user'
+import { projectSubject } from './subjects/project'
+import { organizationSubject } from './subjects/organization'
+import { inviteSubject } from './subjects/invite'
+import { billingSubject } from './subjects/billing'
+import { z } from 'zod'
 
-type AppAbilities = UserSubject | ProjectSubject | ['manage', 'all']
+const appAbilitiesSchema = z.union([
+  projectSubject,
+  userSubject,
+  organizationSubject,
+  inviteSubject,
+  billingSubject,
+  z.tuple([z.literal('manage'), z.literal('all')]),
+])
+
+type AppAbilities = z.infer<typeof appAbilitiesSchema>
 
 export type AppAbility = MongoAbility<AppAbilities>
 export const createAppAbility = createMongoAbility as CreateAbility<AppAbility>
