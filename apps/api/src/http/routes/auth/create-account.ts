b0562ee70a15e9c35e07def4d83f0549418857a5
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2026-01-29 16:15:26 -0300

    feat:Rota cadastro de usuário

--- apps/api/src/http/routes/auth/create-account.ts
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -1,3 +1,5 @@
+import { prisma } from '@/lib/prisma'
+import { hash } from 'bcryptjs'
 import type { FastifyInstance } from 'fastify'
 import type { ZodTypeProvider } from 'fastify-type-provider-zod'
 import { z } from 'zod'
@@ -9,13 +11,37 @@ export async function createAccount(app: FastifyInstance) {
       schema: {
         body: z.object({
           name: z.string(),
-          email: z.string().email(),
+          email: z.email(),
           password: z.string().min(6),
         }),
       },
     },
-    () => {
-      return 'User created!'
+    async (request, reply) => {
+      const { name, email, password } = request.body
+
+      const userWithSameEmail = await prisma.user.findUnique({
+        where: {
+          email,
+        },
+      })
+
+      if (userWithSameEmail) {
+        return reply
+          .status(400)
+          .send({ message: 'User with same e-mail already exists.' })
+      }
+
+      const passwordHash = await hash(password, 6)
+
+      await prisma.user.create({
+        data: {
+          name,
+          email,
+          passwordHash,
+        },
+      })
+
+      return reply.status(201).send()
     }
   )
 }
