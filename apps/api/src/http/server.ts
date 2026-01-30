3fb16d23dfca36808d6cdd27525fd322dfe7e28d
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2026-01-30 18:56:35 -0300

    feat:Error Handling no Fastify

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -13,11 +13,14 @@ import { createAccount } from './routes/auth/create-account'
 import fastifyJwt from '@fastify/jwt'
 import { authenticateWithPassword } from './routes/auth/authenticate-with-password'
 import { getProfile } from './routes/auth/get-profile'
+import { errorHandler } from './error-handler'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
+
+app.setErrorHandler(errorHandler)
 
 app.register(fastifySwagger, {
   openapi: {
