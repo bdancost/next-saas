dca9a5ca6be2f517546144ae54d5457ba61801a0
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2026-02-08 17:15:23 -0300

    feat:Component Header

---
+++ apps/web/src/components/header.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image'
+
+import rocketseatIcon from '@/assets/rocketseat-icon.svg'
+
+import { ProfileButton } from '../components/profile-button'
+
+export function Header() {
+  return (
+    <div className="mx-auto flex max-w-300 items-center justify-between">
+      <div className="flex items-center gap-3">
+        <Image
+          src={rocketseatIcon}
+          className="size-6 dark:invert"
+          alt="Rocketseat"
+        />
+      </div>
+
+      <div className="flex items-center gap-4">
+        <ProfileButton />
+      </div>
+    </div>
+  )
+}
