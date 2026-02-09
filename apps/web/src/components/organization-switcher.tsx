56c20fb27dbfb5d16b00ef57c7f606a56c5ee40f
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2026-02-09 08:31:10 -0300

    Exibindo organizaçao ativa

--- apps/web/src/components/organization-switcher.tsx
+++ apps/web/src/components/organization-switcher.tsx
@@ -1,4 +1,5 @@
 import { ChevronsUpDown, PlusCircle } from 'lucide-react'
+import { cookies } from 'next/headers'
 import Link from 'next/link'
 
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
@@ -14,11 +15,31 @@ import {
 import { getOrganizations } from '@/http/get-organizations'
 
 export async function OrganizationSwitcher() {
+  const cookieStore = await cookies()
+  const currentOrg = cookieStore.get('org')?.value
+
   const { organizations } = await getOrganizations()
+  const currentOrganization = organizations.find(
+    (org) => org.slug === currentOrg
+  )
   return (
     <DropdownMenu>
       <DropdownMenuTrigger className="focus-visible:ring-primary flex w-42 items-center gap-2 rounded p-1 text-sm font-medium outline-none focus-visible:ring-2">
-        <span className="text-muted-foreground">Select organization</span>
+        {currentOrganization ? (
+          <>
+            <Avatar className="mr-2 size-4">
+              {currentOrganization.avatarUrl && (
+                <AvatarImage src={currentOrganization.avatarUrl} />
+              )}
+              <AvatarFallback />
+            </Avatar>
+            <span className="truncate text-left">
+              {currentOrganization.name}
+            </span>
+          </>
+        ) : (
+          <span className="text-muted-foreground">Select organization</span>
+        )}
         <ChevronsUpDown className="text-muted-foreground ml-auto size-4" />
       </DropdownMenuTrigger>
       <DropdownMenuContent
