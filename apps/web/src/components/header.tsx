d6a51447a0f27db010c2ef0ebc9adf83294e882b
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2026-02-15 16:14:08 -0300

    feat:Interception routes no Next.js and add Providers in the Layout

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -1,14 +1,16 @@
+import { Slash } from 'lucide-react'
 import Image from 'next/image'
 import rocketseatIcon from '@/assets/rocketseat-icon.svg'
+import { ability } from '@/auth/auth'
 import { ProfileButton } from './profile-button'
+import { ProjectSwitcher } from './project-switcher'
 import { Separator } from '@/components/ui/separator' //
 import { ThemeSwitcher } from './theme/theme-switcher'
-import { Slash } from 'lucide-react'
 import { OrganizationSwitcher } from './organization-switcher'
-import { ability } from '@/auth/auth'
 
 export async function Header() {
   const permissions = await ability()
+
   return (
     <header className="border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="mx-auto flex max-w-[1200px] items-center justify-between border-b pb-2">
@@ -23,14 +25,12 @@ export async function Header() {
 
           <OrganizationSwitcher />
 
-          {permissions?.can('get', 'Project') && <p>Project</p>}
-
-          <div className="flex items-center gap-4">
-            <span className="text-lg font-semibold text-muted-foreground/50">
-              /
-            </span>
-            <span className="text-sm font-medium">Select organization</span>
-          </div>
+          {permissions?.can('get', 'Project') && (
+            <>
+              <Slash className="size-3 -rotate-[24deg] text-border" />
+              <ProjectSwitcher />
+            </>
+          )}
         </div>
 
         <div className="flex items-center gap-4">
