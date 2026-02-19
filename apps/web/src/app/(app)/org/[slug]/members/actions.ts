d9d5762f943449abeab319d4b528efc060fc867b
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2026-02-19 18:56:24 -0300

    feat:Atualizar cargo do membro

--- apps/web/src/app/(app)/org/[slug]/members/actions.ts
+++ apps/web/src/app/(app)/org/[slug]/members/actions.ts
@@ -4,6 +4,8 @@ import { revalidateTag } from 'next/cache'
 
 import { getCurrentOrg } from '@/auth/auth'
 import { removeMember } from '@/http/remove-member'
+import { Role } from '@saas/auth'
+import { updateMember } from '@/http/update-member'
 
 export async function removeMemberAction(memberId: string) {
   const currentOrg = getCurrentOrg()
@@ -11,6 +13,17 @@ export async function removeMemberAction(memberId: string) {
   await removeMember({
     org: currentOrg!,
     memberId,
+  })
+
+  revalidateTag(`${currentOrg}/members`)
+}
+export async function updateMemberAction(memberId: string, role: Role) {
+  const currentOrg = getCurrentOrg()
+
+  await updateMember({
+    org: currentOrg!,
+    memberId,
+    role,
   })
 
   revalidateTag(`${currentOrg}/members`)
